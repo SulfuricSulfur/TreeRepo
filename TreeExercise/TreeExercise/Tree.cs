9e32443f510ffdd1a0844ccb6ea5d8c0bdcce3e3
Sulfur <rc7604@GDD-3.main.ad.rit.edu>
2016-02-10 18:02:15 -0500

    Based methods for tree
    The core outline of the methods for the tree have been added.

--- TreeExercise/TreeExercise/Tree.cs
+++ TreeExercise/TreeExercise/Tree.cs
@@ -9,8 +9,10 @@ namespace TreeExercise
     //binary tree
     class Tree
     {
+
         //attributes
         private Node root = null;
+        private int numLevels;//the number of levels that the user has inputted
 
         //root value
         public Node Root
@@ -18,9 +20,27 @@ namespace TreeExercise
             get { return root; }
         }
   
+        /// <summary>
+        /// for inserting a node into the tree
+        /// </summary>
+        /// <param name="data"></param>
+        public void Insert(int data)
+        {
+            
+        }
 
+        /// <summary>
+        /// used for going through and finding the next node to the right. The node listed will be the first node the traverse will start at
+        /// (will be set to the root)
+        /// </summary>
+        /// <param name="node"></param>
+        public void Traverse(Node node)
+        {
+            if(node != null)
+            {
 
-
+            }
+        }
 
 
     }
