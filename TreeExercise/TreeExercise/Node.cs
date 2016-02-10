14919082a32a041b1f9e11d410ec17716d1d89f5
Sulfur <rc7604@GDD-3.main.ad.rit.edu>
2016-02-10 16:59:52 -0500

    Node content
    Some attributes have been added to the node class(Left, Right, and
    data). Specific description of each are in the comments.  Left and Right
    nodes start out null. Will set them once created.

--- TreeExercise/TreeExercise/Node.cs
+++ TreeExercise/TreeExercise/Node.cs
@@ -8,5 +8,37 @@ namespace TreeExercise
 {
     class Node
     {
+        //attributes
+        private Node left;//node to the left of the current node
+        private Node right;//node to the right of the current node
+        private int data;//the number being stored
+
+        //constructor
+        //the node will take in a data value that corresponds to it
+        public Node(int value)
+        {
+            data = value;
+            left = null;
+            right = null;
+        }
+
+
+        //properties
+        public int Data
+        {
+            get { return data; }
+        }
+        public Node Left//this is for accessing the node of the left of the current one
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        public Node Right//this is for accessing the node to the right of the current one
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+
     }
 }
