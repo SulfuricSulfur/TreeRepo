f04f4a8c62cf79cf65dbc01b49d5e39cdde916d9
Sulfur <rc7604@GDD-3.main.ad.rit.edu>
2016-02-10 19:39:16 -0500

    Loop
    Working on the for loops, arrays, and list inside the Insert method.
    Trying to save the previous node(root is an exception, it is different)
    in the array, and then added to the list.

--- TreeExercise/TreeExercise/Tree.cs
+++ TreeExercise/TreeExercise/Tree.cs
@@ -32,17 +32,27 @@ namespace TreeExercise
         public void Insert()
         {
             Node currNode = null;//this is the current node that is being addressed.
-
+   
             //setting up for loop for the amount of levels
             for(int i =0; i < numLevels; i++)
             {
+                List<Node> parentNodes = new List<Node>();
+        
                 //this is for the amount of nodes on each level
                 for(int j =1; i < (2^(j-1));j++)
                 {
                     //creating an array of nodes that will hold all the nodes on that current level
-                    Node[] levelNodes;
-                    levelNodes = new Node[(2 ^(j - 1)) + 2];//the reason for the +2 is for the 0's at index 0 and at the very last index (acting like there is nothing to the left or right)
-                
+                    int slots = (2 ^ (j - 1)) + 2;//the number of slots in the array
+                    Node[] levelNodesC = new Node[slots];//the reason for the +2 is for the 0's at index 0 and at the very last index (acting like there is nothing to the left or right)
+                    levelNodesC[0] = new Node(0);//adding a node with value of 0 to first spot
+                    levelNodesC[slots-1] = new Node(0);//adding a node with value of 0 to last spot
+
+                    //getting started with the root node
+                    if (i == 0)
+                    {
+
+                    }
+
                 }
 
             }
